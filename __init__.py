efe05733fa5eab64ccca933c8729127a8d77574c
ericklau <erickson.p.lau@gmail.com>
2020-02-24 17:15:44 -0500

    updated with models file using SQLAlchemy

--- __init__.py
+++ __init__.py
@@ -4,9 +4,9 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    # app.config.from_mapping(
-    #     SECRET_KEY=os.environ.get('SECRET_KEY', default='dev'),
-    # )
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get('SECRET_KEY', default='dev'),
+    )
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
